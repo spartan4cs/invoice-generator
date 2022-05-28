f5daa338de4b03aa6a67b2b9571a1250aef2dcc4
Akash <aakashkumbhare@gmail.com>
2022-05-28 19:13:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import InvoiceForm from "./components/InvoiceForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App d-flex flex-column align-items-center justify-content-center w-100">
+        <Container>
+          <InvoiceForm />
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
